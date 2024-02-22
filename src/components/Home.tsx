a5fc73460c6ac6d85a3985517eaa429f73fa5bcb
Anya-gh <Aniket.Singh@warwick.ac.uk>
2024-02-22 14:28:52 +0000

    v0.1

---
+++ src/components/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="flex flex-col mx-10 my-32">
+      <h1 className="tracking-[0.15em] text-white text-3xl font-bold">Aniket Singh</h1>
+      <h2 className="text-purple-600 text-xl font-bold">Incoming Software Engineer at THG</h2>
+    </div>
+  )
+}
